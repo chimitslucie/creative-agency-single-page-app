591a4a02fa503d3f6c968f836131b1741aadea24
chimitslucie <chimitslucie@gmail.com>
2024-11-11 11:05:42 +0100

    add burger menu

--- src/Components/TopBar.jsx
+++ src/Components/TopBar.jsx
@@ -1,11 +1,24 @@
+import { useState } from "react"
 import logo from "../Assets/Images/logo/logo.svg"
 import Navbar from "./Navbar"
 
 function TopBar() {
+
+    const [showNav, setShowNav] = useState(false);
+
+    const handleShowNav = () => {
+        setShowNav(!showNav)
+    }
+
     return (
-        <div className="topBar">
+        <div className={`topBar ${showNav ? 'showNav' : ""}`}>
             <img src={logo} alt="logo-creative" className="topBarLogo" />
             <Navbar />
+            <button className="topBarBurger" onClick={handleShowNav}>
+                <span className="burgerBar"></span>
+                <span className="burgerBar"></span>
+                <span className="burgerBar"></span>
+            </button>
         </div>
     )
 }
